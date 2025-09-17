51f6bcb56ff1a02bad0189737d43bd34c5691cd9
Higor Bezerra <83132987+Chapeu-Preto@users.noreply.github.com>
2025-09-17 15:50:02 -0300

    Update ft_strcpy.c
    realizada a adição do dest[i] = '\0';

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -16,11 +16,13 @@ char	*ft_strcpy(char *dest, char *src)
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
+	return dest;
 }
 /*
 int	main(void)
